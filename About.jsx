42c8d73b14fde066473fe70fd17349c9850b103c
Arai Hardy <araih@middlebury.edu>
2025-12-04 03:18:45 -0500

    changed for demo

--- About.jsx
+++ About.jsx
@@ -2,7 +2,10 @@ export default function About() {
   return (
     <div>
       <h1>About</h1>
-      <p>Quantum Entanglement Game by Arai Hardy.</p>
+      <p>My intention with this is a Quantum Entanglement Game that
+        can be used as practice to expand equations and see if they are 
+        entanged or in product state; for the sake of deploying something
+        for this demo, here is my site so far. - Arai Hardy.</p>
     </div>
   );
 }
--- Instructions.jsx
+++ Instructions.jsx
@@ -2,8 +2,7 @@ export default function Instructions() {
   return (
     <div>
       <h1>Instructions</h1>
-      <p>Click PLAY on the Home page to view the quantum game code.</p>
-      <p>This website is a React wrapper that displays your Python Tkinter game code.</p>
+      <p>Click PLAY on the Home page once you hve finished the instructions and read the book</p>
     </div>
   );
 }
