04c0936af429a2af715158e573336cb9775d7e5d
Arai Hardy <araih@middlebury.edu>
2025-12-04 03:23:19 -0500

    typo

--- Instructions.jsx
+++ Instructions.jsx
@@ -2,7 +2,7 @@ export default function Instructions() {
   return (
     <div>
       <h1>Instructions</h1>
-      <p>Click PLAY on the Home page once you hve finished the instructions and read the book</p>
+      <p>Click PLAY on the Home page once you have finished the instructions and read the book</p>
     </div>
   );
 }
